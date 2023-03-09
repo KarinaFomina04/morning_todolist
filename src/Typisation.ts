112a456942a208c7891669e4a2dae23bb76d1c83
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-03-09 11:14:43 +0100

    Add new functionality: delete, add, change tasks

---
+++ src/Typisation.ts
@@ -0,0 +1,37 @@
+import {v1} from "uuid";
+
+
+export type shoplistType = {
+    [key:string]: ThingsToBuyPropsType[]
+}
+
+
+export type ThingsToBuyPropsType = {
+    id: string,
+    title: string,
+    expectedPrice: string,
+    realPrice: string,
+    inCart: boolean
+}
+
+export type ShopListPropsType = {
+    shopId: string
+    title: string
+    whatToBuy: ThingsToBuyPropsType[]
+    deleteItemShop: (shoplistId: string, itemID: string) => void
+    changeFilter: (shoplistId: string, newFilterValue:FilterType)=>void
+    addtask:(shoplistId:string, newTitle:string)=> void
+    changeCheckBox: (shoplistId: string, itemID: string, checked: boolean) => void
+    filterValue: FilterType
+
+}
+export type lidstpropsType = {
+    id: string
+    title: string
+    filter: FilterType
+}
+
+export type FilterType = "all" | "buy" | "not buy"
+
+
+
